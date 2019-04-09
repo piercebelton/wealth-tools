851c9972cf1fe128fe059f210202075ee253b146
piercebelton <piercebelton@gmail.com>
2019-04-09 13:01:18 -0700

    remove cruft finish home page

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container fluid fill-height class="home-content" style="max-height: 100vh;">
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-4 font-weight-black white--text text-xs-center">WEALTH TOOLS</div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">See the effects of compound interest, instantly.</div>
+            <v-btn class="mt-5 blue darken-4 white--text" to="/main">
+              Get Started!
+            </v-btn>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeContent'
+};
+</script>
+
+<style scoped>
+.home-content {
+    background: url('https://source.unsplash.com/0jKuUj3vUTg');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>
+        <home-content></home-content>
+        <investments-and-debts></investments-and-debts>
+    </span>
+</template>
+
+<script>
+import HomeContent from '@/components/HomeContent';
+
+export default {
+    name: 'home',
+    components: {
+        HomeContent
+    }
+};
+</script>
