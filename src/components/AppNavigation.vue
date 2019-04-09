299602d58f5b0d91abf297fd4323b9007f7ab044
piercebelton <piercebelton@gmail.com>
2019-04-09 11:19:29 -0700

    add navbar remove some cruft

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+    <span>
+        <v-toolbar app color="blue darken-4" dark>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+        </v-toolbar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Wealth Tools'
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
